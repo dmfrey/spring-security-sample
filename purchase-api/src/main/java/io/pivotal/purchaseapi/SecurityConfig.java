bcbb821c0c8b8c48d6fc739cf3ca12efc2412554
Daniel Frey <dmfrey@gmail.com>
2021-04-30 15:10:11 -0400

    disable csrf in resource server

--- purchase-api/src/main/java/io/pivotal/purchaseapi/SecurityConfig.java
+++ purchase-api/src/main/java/io/pivotal/purchaseapi/SecurityConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.config.web.server.ServerHttpSecurity.CsrfSpec;
 import org.springframework.security.config.web.server.ServerHttpSecurity.OAuth2ResourceServerSpec;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
@@ -18,7 +17,7 @@ public class SecurityConfig {
 
         return http
 
-                .csrf( CsrfSpec::disable )
+//                .csrf( CsrfSpec::disable )
 
 //                .requestCache( requestCache ->
 //                        requestCache.requestCache( NoOpServerRequestCache.getInstance() )
